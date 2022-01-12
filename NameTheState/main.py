8548fd1c6bdad3e335e973595d6c42a70fda27f9
Usman Haroon <haroonasr@gmail.com>
2022-01-12 10:27:42 +0530

    Create missed states csv file

--- NameTheState/main.py
+++ NameTheState/main.py
@@ -9,15 +9,20 @@ turtle.hideturtle()
 turtle.penup()
 
 data = pandas.read_csv('50_states.csv')
-
-answer = ""
+answer=""
+found_states = []
 while answer != 'Exit':
     answer = screen.textinput("Guess the state", "Enter the state name").title()
-    screen.onscreenclick
     if answer == 'Exit':
         break
     states = data.state.to_list()
     if answer in states:
+        found_states.append(answer)
         coord = data.loc[data['state'] == answer]
         turtle.goto(int(coord.x),int(coord.y))
         turtle.write(f'{answer}')
+
+missed_states = [state for state in states if state not in found_states]
+
+missed_states_df = pandas.DataFrame(missed_states,columns=["Missed states"])
+missed_states_df.to_csv('states_to_learn.csv')
